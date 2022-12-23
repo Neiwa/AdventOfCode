1271303fca807833f09394248d7ff89c4a4b1765
Martin Böninen <martin@boninen.se>
2022-12-23 07:51:03 +0100

    Day 21 Part One

---
+++ AdventOfCode2022/Day21/Day21.cs
@@ -0,0 +1,88 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day21
+{
+    public class Day21 : BaseAoc
+    {
+        class Monkey
+        {
+            public long? Number { get; set; }
+
+            public char Operation { get; set; }
+
+            public string LeftMonkey { get; set; }
+
+            public string RightMonkey { get; set; }
+
+            public long Yell(Dictionary<string, Monkey> monkies)
+            {
+                switch (Operation)
+                {
+                    case '+':
+                        Number = Number ?? monkies[LeftMonkey].Yell(monkies) + monkies[RightMonkey].Yell(monkies);
+                        break;
+                    case '-':
+                        Number = Number ?? monkies[LeftMonkey].Yell(monkies) - monkies[RightMonkey].Yell(monkies);
+                        break;
+                    case '*':
+                        Number = Number ?? monkies[LeftMonkey].Yell(monkies) * monkies[RightMonkey].Yell(monkies);
+                        break;
+                    case '/':
+                        Number = Number ?? monkies[LeftMonkey].Yell(monkies) / monkies[RightMonkey].Yell(monkies);
+                        break;
+                }
+                return Number.Value;
+            }
+        }
+
+        Dictionary<string, Monkey> ParseInput(List<string> lines)
+        {
+            Dictionary<string, Monkey> monkies = new();
+
+            foreach (var line in lines)
+            {
+                var match = Regex.Match(line, @"(?<name>\w+): ((?<number>\d+)|(?<left>\w+) (?<op>.) (?<right>\w+))");
+                if (match.Success)
+                {
+                    var name = match.Groups["name"].Value;
+                    if (match.Groups["number"].Success)
+                    {
+                        monkies.Add(name, new Monkey
+                        {
+                            Number = long.Parse(match.Groups["number"].Value)
+                        });
+                    }
+                    else
+                    {
+                        monkies.Add(name, new Monkey
+                        {
+                            LeftMonkey = match.Groups["left"].Value,
+                            RightMonkey = match.Groups["right"].Value,
+                            Operation = match.Groups["op"].Value.First()
+                        });
+                    }
+                }
+            }
+
+            return monkies;
+        }
+
+        public override string PartOne(List<string> lines)
+        {
+            var monkies = ParseInput(lines);
+
+            return monkies["root"].Yell(monkies).ToString();
+        }
+
+        public override string PartTwo(List<string> lines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
