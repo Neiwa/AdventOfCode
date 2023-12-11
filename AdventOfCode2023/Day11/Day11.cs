28fbcf39ad456187c4edb6a60e65e6124f341728
Martin Böninen <niteniten58@gmail.com>
2023-12-11 06:57:11 +0100

    day 11

---
+++ AdventOfCode2023/Day11/Day11.cs
@@ -0,0 +1,51 @@
+﻿using Core;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day11
+{
+    public class Day11 : BaseAoc<Grid>
+    {
+        long GetDistance(Grid input, long age)
+        {
+            var colWeights = Enumerable.Repeat(age, input.Width).ToList();
+            var rowWeights = Enumerable.Repeat(age, input.Height).ToList();
+
+            var galaxies = input.Where(c => c.Value == '#').ToList();
+            foreach (var galaxy in galaxies)
+            {
+                colWeights[galaxy.X] = 1;
+                rowWeights[galaxy.Y] = 1;
+            }
+
+            var res = galaxies.Sum((leftGalaxy) => galaxies
+            .Where(rightGalaxy => rightGalaxy.Y > leftGalaxy.Y || (rightGalaxy.Y == leftGalaxy.Y && rightGalaxy.X > leftGalaxy.X))
+            .Sum(rightGalaxy =>
+                (leftGalaxy.X > rightGalaxy.X ?
+                    colWeights.Skip(rightGalaxy.X + 1).Take(leftGalaxy.X - rightGalaxy.X).Sum() :
+                    colWeights.Skip(leftGalaxy.X + 1).Take(rightGalaxy.X - leftGalaxy.X).Sum()) +
+                rowWeights.Skip(leftGalaxy.Y + 1).Take(rightGalaxy.Y - leftGalaxy.Y).Sum()));
+
+            return res;
+        }
+
+        public override string PartOne(Grid input)
+        {
+            return GetDistance(input, 2).ToString();
+        }
+
+        public override string PartTwo(Grid input)
+        {
+            return GetDistance(input, 1_000_000).ToString();
+        }
+
+        protected override Grid ParseInput(List<string> lines)
+        {
+            return lines.ToGrid();
+        }
+    }
+}
