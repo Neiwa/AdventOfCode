992d4e7645da3bc39fbc4a4b53f5ce86350f619d
Martin Böninen <niteniten58@gmail.com>
2023-12-12 07:30:57 +0100

    day 12 part 1

---
+++ AdventOfCode2023/Day12/Day12.cs
@@ -0,0 +1,128 @@
+﻿using Core;
+using Helpers;
+
+namespace AdventOfCode2023.Day12
+{
+    [TestFixture]
+    public class Day12 : BaseAocV2
+    {
+        [TestCase("#.#.### 1,1,3")]
+        [TestCase(".#...#....###. 1,1,3")]
+        [TestCase(".#.###.#.###### 1,3,1,6")]
+        [TestCase("####.#...#... 4,1,1")]
+        [TestCase("#....######..#####. 1,6,5")]
+        [TestCase(".###.##....# 3,2,1")]
+        public void IsValid_Tests(string input)
+        {
+            var ss = input.Split(' ');
+            var groups = ss[1].Split(',').Select(int.Parse).ToList();
+
+            var actual = IsValid(ss[0], groups);
+
+            Assert.That(actual, Is.True);
+        }
+
+        public bool IsValid(string record, List<int> groups, bool unknownIsSuccess = false)
+        {
+            int curGroup = 0;
+            int count = 0;
+
+            var trimmedRecord = record.Trim('.') + ".";
+
+            foreach (var spring in trimmedRecord)
+            {
+                if (spring == '.' && count > 0)
+                {
+                    if (count != groups[curGroup])
+                    {
+                        return false;
+                    }
+
+                    curGroup++;
+                    count = 0;
+                }
+                else if (spring == '#')
+                {
+                    count++;
+                }
+                else if (unknownIsSuccess && spring == '?')
+                {
+                    return true;
+                }
+            }
+
+            return true;
+        }
+
+        public override string PartOne(List<string> lines)
+        {
+            var sum = 0L;
+
+            foreach (var line in lines)
+            {
+                var ss = line.Split(' ');
+                var groups = ss[1].Split(',').Select(int.Parse).ToList();
+                var known = ss[0].Count(c => c == '#');
+                var missing = groups.Sum() - known;
+
+                var unknownPositions = ss[0].AllIndexesOf("?").ToHashSet();
+
+                foreach (var subSet in Enumerations.GetAllSubSets(unknownPositions).Where(set => set.Count == missing))
+                {
+                    var record = ss[0].Replace('?', '.').ToArray();
+                    foreach (var pos in subSet)
+                    {
+                        record[pos] = '#';
+                    }
+
+                    var updatedRecord = new string(record);
+                    if (IsValid(updatedRecord, groups))
+                    {
+                        WriteLine(updatedRecord);
+                        sum++;
+                    }
+                }
+            }
+
+            return sum.ToString();
+        }
+
+        public override string PartTwo(List<string> lines)
+        {
+            var sum = 0L;
+
+            foreach (var line in lines)
+            {
+                var ss = line.Split(' ');
+                var groups = ss[1].Split(',').Select(int.Parse).ToList();
+
+                var unfoldedGroups = groups.Concat(groups).Concat(groups).Concat(groups).Concat(groups).ToList();
+
+                var unfoldedRecord = $"{ss[0]}?{ss[0]}?{ss[0]}?{ss[0]}?{ss[0]}";
+
+                var known = unfoldedRecord.Count(c => c == '#');
+                var missing = groups.Sum() - known;
+
+                var unknownPositions = unfoldedRecord.AllIndexesOf("?").ToHashSet();
+
+                foreach (var subSet in Enumerations.GetAllSubSets(unknownPositions).Where(set => set.Count == missing))
+                {
+                    var record = unfoldedRecord.Replace('?', '.').ToArray();
+                    foreach (var pos in subSet)
+                    {
+                        record[pos] = '#';
+                    }
+
+                    var updatedRecord = new string(record);
+                    if (IsValid(updatedRecord, groups))
+                    {
+                        WriteLine(updatedRecord);
+                        sum++;
+                    }
+                }
+            }
+
+            return sum.ToString();
+        }
+    }
+}
--- Helpers/Enumerations.cs
+++ Helpers/Enumerations.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Helpers
+﻿namespace Helpers
 {
     public static class Enumerations
     {
@@ -51,10 +45,20 @@ namespace Helpers
             while (true)
             {
                 yield return input[pos++];
-                if(pos == input.Length)
+                if (pos == input.Length)
                 {
                     pos = 0;
                 }
+            }
+        }
+
+        public static IEnumerable<int> AllIndexesOf(this string str, string searchstring)
+        {
+            int minIndex = str.IndexOf(searchstring);
+            while (minIndex != -1)
+            {
+                yield return minIndex;
+                minIndex = str.IndexOf(searchstring, minIndex + searchstring.Length);
             }
         }
     }
