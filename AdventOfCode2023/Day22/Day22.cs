c9f392545f6234cba66a9a94bb62d4540304c6e6
Martin Böninen <niteniten58@gmail.com>
2023-12-23 07:39:30 +0100

    day 22 part 2

--- AdventOfCode2023/Day22/Day22.cs
+++ AdventOfCode2023/Day22/Day22.cs
@@ -1,5 +1,6 @@
 ﻿using Core;
 using Helpers;
+using Helpers.Extensions;
 using Helpers.Grid;
 using System.Text.RegularExpressions;
 
@@ -132,7 +133,134 @@ namespace AdventOfCode2023.Day22
 
         public override string PartTwo(List<string> lines)
         {
-            throw new NotImplementedException();
+            var bricks = lines.Select(l =>
+            {
+                var matches = Regex.Matches(l, @"(?<x>\d+),(?<y>\d+),(?<z>\d+)");
+
+                return new Cube(
+                    new LongPoint3D(
+                        long.Parse(matches[0].Groups["x"].Value),
+                        long.Parse(matches[0].Groups["y"].Value),
+                        long.Parse(matches[0].Groups["z"].Value)),
+                    new LongPoint3D(
+                        long.Parse(matches[1].Groups["x"].Value),
+                        long.Parse(matches[1].Groups["y"].Value),
+                        long.Parse(matches[1].Groups["z"].Value)));
+            }).OrderBy(c => Math.Min(c.FirstCorner.Z, c.SecondCorner.Z)).ToList();
+
+            var restingOn = new ValueCreationDictionary<int, List<int>>();
+            var supporting = new ValueCreationDictionary<int, List<int>>();
+
+            var topDownGrid = new SparseGrid<(long Height, int Brick)>((0, -1));
+
+            for (var i = 0; i < bricks.Count; i++)
+            {
+                var brick = bricks[i];
+
+                var bottomPoints = new List<LongPoint3D>();
+                if (brick.FirstCorner.Z == brick.SecondCorner.Z)
+                {
+                    // Lying down
+                    Point shift;
+                    if (brick.FirstCorner.X == brick.SecondCorner.X)
+                    {
+                        if (brick.FirstCorner.Y > brick.SecondCorner.Y)
+                        {
+                            shift = new Point(0, -1);
+                        }
+                        else
+                        {
+                            shift = new Point(0, 1);
+                        }
+                    }
+                    else
+                    {
+                        if (brick.FirstCorner.X > brick.SecondCorner.X)
+                        {
+                            shift = new Point(-1, 0);
+                        }
+                        else
+                        {
+                            shift = new Point(1, 0);
+                        }
+                    }
+
+                    var curr = new Point(brick.FirstCorner.X, brick.FirstCorner.Y);
+                    var target = new Point(brick.SecondCorner.X, brick.SecondCorner.Y);
+                    bottomPoints.Add(new LongPoint3D(curr, brick.FirstCorner.Z));
+                    while (curr != target)
+                    {
+                        curr += shift;
+                        bottomPoints.Add(new LongPoint3D(curr, brick.FirstCorner.Z));
+                    }
+                }
+                else
+                {
+                    // Standing up
+                    bottomPoints.Add(new LongPoint3D(brick.FirstCorner.X, brick.FirstCorner.Y, Math.Min(brick.FirstCorner.Z, brick.SecondCorner.Z)));
+                    bottomPoints.Add(new LongPoint3D(brick.FirstCorner.X, brick.FirstCorner.Y, Math.Max(brick.FirstCorner.Z, brick.SecondCorner.Z)));
+                }
+
+                var highestPoint = bottomPoints.MaxBy(p => topDownGrid.ValueAt(p.X, p.Y).Height);
+                var height = topDownGrid.ValueAt(highestPoint.X, highestPoint.Y).Height;
+                var diff = new LongPoint3D(0, 0, highestPoint.Z - height - 1);
+
+                bottomPoints = bottomPoints.Select(p => p - diff).ToList();
+
+                var bricksBelow = bottomPoints.Select(p => topDownGrid.ValueAt(p.X, p.Y)).Where(v => v.Height == bottomPoints[0].Z - 1).Select(v => v.Brick).Distinct();
+
+                restingOn[i].AddRange(bricksBelow);
+                foreach (var brickBelow in bricksBelow)
+                {
+                    supporting[brickBelow].Add(i);
+                }
+
+                foreach (var bottomPoint in bottomPoints)
+                {
+                    topDownGrid.At(bottomPoint.X, bottomPoint.Y).Value = (bottomPoint.Z, i);
+                }
+
+                Draw(topDownGrid);
+            }
+
+            var supportSum = new Dictionary<int, int>();
+            var chain = new ValueCreationDictionary<int, int>();
+
+            int calcSupportValue(int i)
+            {
+                HashSet<int> removed = new() { i };
+
+                var resting = new Queue<int>();
+
+                var currentBrick = i;
+                var supportValue = 0;
+
+                var freeFloating = supporting[i].Where(brick => !restingOn[brick].Except(removed).Any());
+
+                resting.Enqueue(freeFloating);
+
+                while (resting.TryDequeue(out var brick))
+                {
+                    if (removed.Contains(brick))
+                    {
+                        continue;
+                    }
+
+                    removed.Add(brick);
+                    supportValue++;
+                    freeFloating = supporting[brick].Where(brick => !restingOn[brick].Except(removed).Any());
+                    resting.Enqueue(freeFloating);
+                }
+
+                return supportValue;
+            }
+
+            for (int i = bricks.Count - 1; i >= 0; i--)
+            {
+                chain[i] = calcSupportValue(i);
+            }
+
+            return chain.Values.Sum().ToString();
         }
     }
 }
---
+++ Helpers/Extensions/QueueExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Helpers.Extensions
+{
+    public static class QueueExtensions
+    {
+        public static void Enqueue<T>(this Queue<T> queue, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                queue.Enqueue(item);
+            }
+        }
+        public static void Enqueue<T>(this Queue<T> queue, params T[] items)
+        {
+            foreach (var item in items)
+            {
+                queue.Enqueue(item);
+            }
+        }
+    }
+}
