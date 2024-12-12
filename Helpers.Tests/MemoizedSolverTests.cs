bf3a0339268de9092638e0af9e2b9c000c1143e1
Martin Böninen <niteniten58@gmail.com>
2024-12-12 21:19:05 +0100

    add memoized-solver

---
+++ Helpers.Tests/MemoizedSolverTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Tests
+{
+    public class MemoizedSolverTests
+    {
+
+        public record State(int Number);
+
+        public record Result(int Number);
+        [Test]
+        public void Solve_Fibonacci()
+        {
+            var solver = new MemoizedSolver<State, Result>();
+
+            var result = solver.Solve((callback, state) =>
+            {
+                if (state.Number == 0)
+                {
+                    return new Result(0);
+                }
+                if (state.Number == 1)
+                {
+                    return new Result(1);
+                }
+
+                return new Result(callback(new State(state.Number - 2)).Number + callback(new State(state.Number - 1)).Number);
+
+            }, new State(6));
+            
+            Assert.That(result.Number, Is.EqualTo(8));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(6, 8)]
+        [TestCase(10, 55)]
+        [TestCase(15, 610)]
+        [TestCase(50, 12586269025)]
+        [TestCase(90, 2880067194370816120)]
+        public void Solve_Fibonacci2(long input, long expected)
+        {
+            var solver = new MemoizedSolver<long, long>();
+
+            var result = solver.Solve((callback, state) =>
+            {
+                if (state == 0)
+                {
+                    return 0;
+                }
+                if (state == 1)
+                {
+                    return 1;
+                }
+
+                return callback(state - 2) + callback(state - 1);
+
+            }, input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(6, 8)]
+        [TestCase(10, 55)]
+        [TestCase(15, 610)]
+        [TestCase(30, 832040)]
+        public void Solve_Fibonacci3(long input, long expected)
+        {
+            var solver = new MemoizedSolver<long, long>();
+
+            long fib (long state)
+            {
+                if (state == 0)
+                {
+                    return 0;
+                }
+                if (state == 1)
+                {
+                    return 1;
+                }
+
+                return fib(state - 2) + fib(state - 1);
+
+            }
+
+            Assert.That(fib(input), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Helpers/MemoizedSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class MemoizedSolver<TState, TResult>
+    {
+        private readonly Dictionary<TState, TResult> _memoized = [];
+
+        public TResult Solve(Func<Func<TState, TResult>, TState, TResult> function, TState initialState)
+        {
+            TResult callback(TState state)
+            {
+                return solve(function, state);
+            }
+
+            TResult solve(Func<Func<TState, TResult>, TState, TResult> function, TState state)
+            {
+                if (_memoized.TryGetValue(state, out var memoResult))
+                {
+                    return memoResult;
+                }
+
+                var result = function(callback, state);
+                _memoized[state] = result;
+                return result;
+            }
+
+            return solve(function, initialState);
+        }
+    }
+}
