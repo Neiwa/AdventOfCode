fcdf333036fd762169f5fbc535e4f9870217eb0c
Martin Böninen <niteniten58@gmail.com>
2025-12-04 06:40:55 +0100

    day 4 2025

---
+++ AdventOfCode2025/Day04/Day04.cs
@@ -0,0 +1,40 @@
+﻿using Helpers.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Day04;
+
+public class Day04 : BaseAocV2
+{
+    public override object PartOne(List<string> lines)
+    {
+        var map = lines.ToGrid();
+
+        return map.Count(cell => cell.Value == '@' && cell.GetNeighbours(true).Count(cell => cell.Value == '@') < 4);
+    }
+
+    public override object PartTwo(List<string> lines)
+    {
+        var map = lines.ToGrid();
+        var count = 0;
+        bool removed = false;
+        do
+        {
+            removed = false;
+            foreach (var cell in map)
+            {
+                if (cell.Value == '@' && cell.GetNeighbours(true).Count(cell => cell.Value == '@') < 4)
+                {
+                    count++;
+                    cell.Value = 'x';
+                    removed = true;
+                }
+            }
+        } while (removed);
+
+        return count;
+    }
+}
---
+++ AdventOfCode2025/Day05/Day05.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Day05;
+
+public class Day05 : BaseAocV2
+{
+    public override object PartOne(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object PartTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
