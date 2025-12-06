89eb53a249f5109742caa9a076346eec0d68ee11
Martin Böninen <niteniten58@gmail.com>
2025-12-06 10:42:08 +0100

    day 6 2025

--- AdventOfCode2025/Day06/Day06.cs
+++ AdventOfCode2025/Day06/Day06.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Helpers.Grid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2025.Day06;
@@ -10,11 +12,87 @@ public class Day06 : BaseAocV2
 {
     public override object PartOne(List<string> lines)
     {
-        throw new NotImplementedException();
+        var cells = lines.Select(line => 
+            Regex.Matches(line, @"((?<cell>[\d*+]+)\s*)+")
+            .SelectMany(match => match.Groups["cell"].Captures.Select(c => c.Value)).ToList())
+            .ToList();
+
+        var sum = 0L;
+
+        for (int i = 0; i < cells[0].Count; i++)
+        {
+            switch (cells.Last()[i])
+            {
+                case "+":
+                    sum += cells.Take(cells.Count - 1).Select(row => long.Parse(row[i])).Sum();
+                    break;
+                case "*":
+                    sum += cells.Take(cells.Count - 1).Select(row => long.Parse(row[i])).Aggregate(1L, (agg, val) => agg * val);
+                    break;
+            }
+        }
+
+        return sum;
     }
 
     public override object PartTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        var grid = lines.ToGrid();
+
+        long sum = 0;
+
+        char op = ' ';
+        List<long> numbers = [];
+
+        for (int x = 0; x <= grid.Width; x++)
+        {
+            bool blank = true;
+            var val = "";
+            if (x < grid.Width)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    var c = grid.ValueAt(x, y);
+                    switch (c)
+                    {
+                        case (>= '0') and (<= '9'):
+                            val += c;
+                            blank = false;
+                            break;
+                        case '*':
+                            op = c;
+                            blank = false;
+                            break;
+                        case '+':
+                            op = c;
+                            blank = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            if (blank)
+            {
+                if (IsDebug)
+                {
+                    WriteLine($"{op} {string.Join(' ', numbers)}");
+                }
+
+                sum += op switch  {
+                    '+' => numbers.Sum(),
+                    '*' => numbers.Aggregate(1L, (agg, val) => agg * val),
+                    _ => 0
+                };
+                numbers = [];
+            }
+            else if (val.Length > 0)
+            {
+                numbers.Add(long.Parse(val));
+            }
+        }
+
+        return sum;
     }
 }
---
+++ AdventOfCode2025/Day07/Day07.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Day07;
+
+public class Day07 : BaseAocV2
+{
+    public override object PartOne(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object PartTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
