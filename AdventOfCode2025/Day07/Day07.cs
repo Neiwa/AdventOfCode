951068a2a7186dc80f8bcceead7f8451deb3a0ad
Martin Böninen <niteniten58@gmail.com>
2025-12-07 06:37:46 +0100

    day 7 2025

--- AdventOfCode2025/Day07/Day07.cs
+++ AdventOfCode2025/Day07/Day07.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Helpers.Grid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,74 @@ public class Day07 : BaseAocV2
 {
     public override object PartOne(List<string> lines)
     {
-        throw new NotImplementedException();
+        var map = lines.ToGrid();
+
+        var start = map.First(cell => cell.Value == 'S');
+        var direction = new Point(0, 1);
+        var left = new Point(-1, 0);
+        var right = new Point(1, 0);
+
+        var splits = 0;
+
+        List<GridCellReference<char>> beams = [start];
+
+        for (int i = 1; i < map.Height; i++)
+        {
+            List<GridCellReference<char>> newBeams = [];
+
+            foreach (var beam in beams)
+            {
+                var newBeam = beam + direction;
+                if (newBeam.Value == '^')
+                {
+                    newBeams.AddRange([newBeam + left, newBeam + right]);
+                    splits++;
+                }
+                else
+                {
+                    newBeams.Add(newBeam);
+                }
+            }
+
+            beams = newBeams.DistinctBy(b => b.Point).ToList();
+        }
+
+
+        return splits;
     }
 
     public override object PartTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        var map = lines.ToGrid();
+
+        var start = map.First(cell => cell.Value == 'S');
+        var direction = new Point(0, 1);
+        var left = new Point(-1, 0);
+        var right = new Point(1, 0);
+
+        List<(GridCellReference<char>, long)> beams = [(start, 1)];
+
+        for (int i = 1; i < map.Height; i++)
+        {
+            List<(GridCellReference<char>, long)> newBeams = [];
+
+            foreach (var beam in beams)
+            {
+                var newBeam = beam.Item1 + direction;
+                if (newBeam.Value == '^')
+                {
+                    newBeams.AddRange([(newBeam + left, beam.Item2), (newBeam + right, beam.Item2)]);
+                }
+                else
+                {
+                    newBeams.Add((newBeam, beam.Item2));
+                }
+            }
+
+            beams = newBeams.GroupBy(b => b.Item1.Point).Select(g => (g.First().Item1, g.Sum(b => b.Item2))).ToList();
+        }
+
+
+        return beams.Sum(b => b.Item2);
     }
 }
---
+++ AdventOfCode2025/Day08/Day08.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Day08;
+
+public class Day08 : BaseAocV2
+{
+    public override object PartOne(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object PartTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
